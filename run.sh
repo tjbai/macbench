16cf5954023a0f9b10a4d29b499bc3429803ecac
TJ Bai <t_bai@apple.com>
2024-07-28 16:21:24 -0500

    fix order of operations

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 python3 -m venv venv
-pip3 install torch psutil numpy tqdmnumpy tqdmnumpy tqdmnumpy tqdm
 
 source venv/bin/activate
-
+pip3 install torch psutil numpy tqdmnumpy tqdmnumpy tqdmnumpy tqdm
 python test.py
 
 deactivate
