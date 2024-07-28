fd2d86af3476065f217c063da4ad9716c2dac468
TJ Bai <t_bai@apple.com>
2024-07-28 16:02:56 -0500

    pip3

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python3 -m venv venv
-pip install torch psutil numpy tqdmnumpy tqdmnumpy tqdmnumpy tqdm
+pip3 install torch psutil numpy tqdmnumpy tqdmnumpy tqdmnumpy tqdm
 
 source venv/bin/activate
 
